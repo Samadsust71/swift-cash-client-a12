b3b50993347a013143c3be0890076bda15fea2bd
Samadsust71 <samadsust71@gmail.com>
2025-01-14 02:56:07 +0600

    color and folder structure done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
+import daisyui from "daisyui";
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
     extend: {},
+    colors: {
+      "bg-main": "#1D1E30", // Main background color
+      "brand-primary": "#01D676", // Primary brand color
+      "surface": "#1E333C", // Accent or surface color for UI elements
+      "text-light": "#FFFFFF", // Primary text color on dark backgrounds
+      "text-muted": "#bdbdbd", // Secondary or muted text color
+    },
   },
   plugins: [daisyui],
   daisyui: {
     themes: ["sunset", "dracula"],
   },
-}
-
+};
