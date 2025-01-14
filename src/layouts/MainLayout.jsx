028448c768e5399bec72b69af1bb52ec2fb71f75
Samadsust71 <samadsust71@gmail.com>
2025-01-14 07:25:28 +0600

    added footer component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/shared/Navbar"
+import Footer from "../components/footer/Footer"
 
 
 const MainLayout = () => {
   return (
     <div>
       <Navbar/>
-      <div className="min-h-[calc(100vh-128px)] mt-[66px]">
+      <div className="min-h-[calc(100vh-244px)] mt-[66px]">
         <Outlet/>
       </div>
+      <Footer/>
     </div>
   )
 }
