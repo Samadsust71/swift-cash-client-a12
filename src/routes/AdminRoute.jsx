b3bfa90d023164d32eb3e6d86f6f97cb86a5bcf6
Samadsust71 <samadsust71@gmail.com>
2025-01-16 00:12:42 +0600

    added admin route

---
+++ src/routes/AdminRoute.jsx
@@ -0,0 +1,19 @@
+import { Navigate } from "react-router-dom"
+import Loading from "../components/Loading"
+import useRole from "../hooks/useRole"
+import PropTypes from "prop-types"
+
+
+const AdminRoute = ({ children }) => {
+    const [role, isLoading] = useRole()
+  
+    if (isLoading) return <Loading />
+    if (role === 'admin') return children
+    return <Navigate to='/dashboard' replace='true' />
+  }
+  
+  AdminRoute.propTypes = {
+    children: PropTypes.element,
+  }
+  
+  export default AdminRoute
