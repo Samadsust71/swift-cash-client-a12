272c44778910e308b3696d760ef69d07c2551114
Samadsust71 <samadsust71@gmail.com>
2025-01-18 00:38:28 +0600

    added buyer states componets and its funcionality

--- src/pages/dashboard/buyer/BuyerHome.jsx
+++ src/pages/dashboard/buyer/BuyerHome.jsx
@@ -1,12 +1,31 @@
-import ReviewTable from "../../../components/buyer/ReviewTable";
-import States from "../../../components/dashboard/States";
+import { useQuery } from "@tanstack/react-query";
+import BuyerStates from "../../../components/buyer/BuyerStates";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
+import Loading from "../../../components/Loading";
+
 
 
 const BuyerHome = () => {
+  const axiosSecure = useAxiosSecure()
+  const {user} = useAuth()
+  const {
+    data: buyerStat = {},
+    isLoading,
+    isError,
+    
+  } = useQuery({
+    queryKey: ["buyer-stat",user?.email],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/buyer-stats/${user?.email}`);
+      return data;
+    },
+  });
+  if(isLoading) return <Loading/>
+  if(isError) console.log("error happend in buyer home", isError)
   return (
     <div className="mt-12">
-      <States />
-      <ReviewTable/>
+      <BuyerStates buyerStat ={buyerStat} />
     </div>
   );
 };
