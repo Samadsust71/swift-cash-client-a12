7f1354e37528ced486db01c39d3003027361d957
Samadsust71 <samadsust71@gmail.com>
2025-01-17 23:45:35 +0600

    admin manage user funcionality done

--- src/pages/dashboard/admin/AdminManageTasks.jsx
+++ src/pages/dashboard/admin/AdminManageTasks.jsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import Loading from "../../../components/Loading";
+import Swal from "sweetalert2";
+import toast from "react-hot-toast";
 
 
 const AdminManageTasks = () => {
@@ -9,6 +11,7 @@ const AdminManageTasks = () => {
     data: allTasks = [],
     isLoading,
     isError,
+    refetch
   } = useQuery({
     queryKey: ["all-tasks"],
     queryFn: async () => {
@@ -18,6 +21,34 @@ const AdminManageTasks = () => {
   });
   if (isLoading) return <Loading />;
   if (isError) console.log("error happend in admin manage tasks page");
+  const handleTaskDelete = (id)=>{
+   
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete the task!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const { data } = await axiosSecure.delete(`/all-tasks/${id}`);
+          if (data?.deletedCount) {
+            Swal.fire({
+              title: "Task Deleted",
+              text: "The task has been deleted.",
+              icon: "success",
+            });
+            refetch();
+          }
+        } catch (error) {
+          toast.error(error?.message || "Some thing went Wrong");
+        }
+      }
+    });
+  }
   return (
     <div className="p-6  rounded-lg shadow-lg my-10 w-full mx-auto">
       <h2 className="text-3xl font-semibold text-center mb-6">All Tasks</h2>
@@ -45,7 +76,7 @@ const AdminManageTasks = () => {
                   <td>{task?.buyer?.name || "N/A"}</td>
                   <td>{task?.buyer?.email}</td>
                   <td>
-                    <button onClick={()=>handleTask(task?._id)} className="text-sm bg-red-100 px-2 py-1 rounded-full text-red-500">
+                    <button onClick={()=>handleTaskDelete(task?._id)} className="text-sm bg-red-100 px-2 py-1 rounded-full text-red-500">
                       Delete
                     </button>
                   </td>
