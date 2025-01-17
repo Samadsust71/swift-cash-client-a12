02991b2f4e6ef626b02892ea19038172c4b9d84a
Samadsust71 <samadsust71@gmail.com>
2025-01-17 08:02:12 +0600

    admin manage user page done

---
+++ src/components/admin-componets/AdminStates.jsx
@@ -0,0 +1,101 @@
+
+import { FaCoins, FaDollarSign, FaUserAlt, FaUsers } from "react-icons/fa"
+
+import PropTypes from 'prop-types';
+
+const AdminStates = ({adminInfo}) => {
+    
+    const {
+        totalAvailableCoins,
+        totalBuyers,
+        totalPayments,
+        totalWorkers
+    } = adminInfo || {}
+  return (
+    <div className="mb-12 grid gap-y-10 gap-x-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 flex-grow">
+      {/*Total Worker */}
+      <div className=" bg-bg-main">
+        <div className="relative flex flex-col bg-clip-border rounded-xl  text-white  shadow-md ">
+          <div
+            className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute mt-2 grid h-16 w-16 place-items-center from-orange-600 to-orange-400 text-white shadow-orange-500/40`}
+          >
+            <FaUsers className="w-6 h-6 text-white" />
+          </div>
+          <div className="p-4 text-right">
+            <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+            Total Worker
+            </p>
+            <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+              {totalWorkers}
+            </h4>
+          </div>
+        </div>
+      </div>
+      {/* total buyer */}
+      <div className=" bg-bg-main">
+        <div className="relative flex flex-col bg-clip-border rounded-xl bg-bg-main text-white  shadow-md ">
+          <div
+            className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute mt-2 grid h-16 w-16 place-items-center from-blue-600 to-blue-400 text-white shadow-blue-500/40`}
+          >
+            <FaUserAlt className="w-6 h-6 text-white" />
+          </div>
+          <div className="p-4 text-right">
+            <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+            Total Buyer
+            </p>
+            <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+              {totalBuyers}
+            </h4>
+          </div>
+        </div>
+      </div>
+      {/* Total Coins */}
+      <div className=" bg-bg-main">
+        <div className="relative flex flex-col bg-clip-border rounded-xl bg-bg-main text-white shadow-md ">
+          <div
+            className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute mt-2 grid h-16 w-16 place-items-center from-pink-600 to-pink-400 text-white shadow-pink-500/40`}
+          >
+            <FaCoins className="w-6 h-6 text-white" />
+          </div>
+          <div className="p-4 text-right">
+            <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+              Total Coin
+            </p>
+            <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+              {totalAvailableCoins}
+            </h4>
+          </div>
+        </div>
+      </div>
+      {/* total payments */}
+      <div className=" bg-bg-main">
+        <div className="relative flex flex-col bg-clip-border rounded-xl bg-bg-main text-white shadow-md ">
+          <div
+            className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute mt-2 grid h-16 w-16 place-items-center from-green-600 to-green-400 text-white shadow-green-500/40`}
+          >
+            <FaDollarSign className="w-6 h-6 text-white" />
+          </div>
+          <div className="p-4 text-right">
+            <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+              Total Payment
+            </p>
+            <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
+              ${totalPayments}
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+AdminStates.propTypes = {
+  adminInfo: PropTypes.shape({
+    totalAvailableCoins: PropTypes.number,
+    totalBuyers: PropTypes.number,
+    totalPayments: PropTypes.number,
+    totalWorkers: PropTypes.number,
+  })
+};
+
+export default AdminStates;
+
--- src/pages/dashboard/admin/AdminHome.jsx
+++ src/pages/dashboard/admin/AdminHome.jsx
@@ -1,19 +1,78 @@
-
+import { useQuery } from "@tanstack/react-query";
+import AdminStates from "../../../components/admin-componets/AdminStates";
 import Loading from "../../../components/Loading";
 import useAdmin from "../../../hooks/useAdmin";
-
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const AdminHome = () => {
-  
-  const [adminInfo,isLoading] = useAdmin()
-  
-  if(isLoading) return <Loading/>
-  console.log(adminInfo.totalWorkers)
+  const [adminInfo, isLoading] = useAdmin();
+  const axiosSecure = useAxiosSecure();
+  const {
+    data: withdrawRequests = [],
+    isLoading: loading,
+    isError,
+  } = useQuery({
+    queryKey: ["withdraw-Requests"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/withdrawals-request`);
+      return data;
+    },
+  });
+
+  if (isLoading || loading) return <Loading />;
+  if (isError) console.log("error in admin home", isError);
   return (
     <div>
-      Admin Home
-    </div>
-  )
-}
+      <AdminStates adminInfo={adminInfo} />
 
-export default AdminHome
+      <div className="p-6  rounded-lg shadow-lg my-10 w-full mx-auto">
+        <h2 className="text-3xl font-semibold text-center mb-6">Withdrawals Request</h2>
+        <div className="divider"></div>
+        <div className="overflow-x-auto">
+          {withdrawRequests && withdrawRequests.length ? (
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>Worker Name</th>
+                  <th>Payment Method</th>
+                  <th>Amount</th>
+                  <th>Reduced Coin</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {withdrawRequests.map((request) => (
+                  <tr key={request?._id}>
+                    <th>{request?.worker_name}</th>
+                    <td>{request?.payment_system}</td>
+                    <td>${request?.withdrawal_amount}</td>
+                    <td>{request?.withdrawal_coin}</td>
+
+                    <td>
+                      <button className="text-sm bg-red-100 px-2 py-1 rounded-full text-red-500">
+                        {request?.status}
+                      </button>
+                    </td>
+                    <td>
+                      <button className="text-sm bg-green-100 px-2 py-1 rounded-full text-green-500">
+                        Payment Success
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <div className="flex justify-center items-center my-5">
+              <p className="text-center">No Withdrawals Request</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminHome;
