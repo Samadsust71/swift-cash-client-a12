cde65f3ec60189c6317a16b5468898d0dbc512e3
Samadsust71 <samadsust71@gmail.com>
2025-01-18 05:23:26 +0600

    worker states component done

--- src/components/buyer/BuyerStates.jsx
+++ src/components/buyer/BuyerStates.jsx
@@ -1,73 +1,68 @@
-
 import { FaDollarSign, FaTasks } from "react-icons/fa";
 import { MdOutlinePendingActions } from "react-icons/md";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-
-
-const BuyerStates = ({buyerStat}) => {
-   
-    const {pendingTaskCount,totalPayments,totalTaskCount} = buyerStat || {}
+const BuyerStates = ({ buyerStat }) => {
+  const { pendingTaskCount, totalPayments, totalTaskCount } = buyerStat || {};
   return (
-    <div className='mb-12 grid gap-y-10 gap-x-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-3  flex-grow'>
-        {/* Sales Card */}
-       <div className=" bg-bg-main">
-       <div className='relative flex flex-col bg-clip-border rounded-xl  text-white  shadow-md '>
+    <div className="mb-12 grid gap-y-10 gap-x-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-3  flex-grow">
+      {/* Sales Card */}
+      <div className=" bg-bg-main">
+        <div className="relative flex flex-col bg-clip-border rounded-xl  text-white  shadow-md ">
           <div
             className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute mt-2 grid h-16 w-16 place-items-center from-orange-600 to-orange-400 text-white shadow-orange-500/40`}
           >
-            <FaTasks  className='w-6 h-6 text-white' />
+            <FaTasks className="w-6 h-6 text-white" />
           </div>
-          <div className='p-4 text-right'>
-            <p className='block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600'>
-            Total Task
+          <div className="p-4 text-right">
+            <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+              Total Task
             </p>
-            <h4 className='block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900'>
+            <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
               {totalTaskCount}
             </h4>
           </div>
         </div>
-       </div>
-        {/* Pending task */}
-       <div className=" bg-bg-main">
-       <div className='relative flex flex-col bg-clip-border rounded-xl bg-bg-main text-white  shadow-md '>
+      </div>
+      {/* Pending task */}
+      <div className=" bg-bg-main">
+        <div className="relative flex flex-col bg-clip-border rounded-xl bg-bg-main text-white  shadow-md ">
           <div
             className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute mt-2 grid h-16 w-16 place-items-center from-blue-600 to-blue-400 text-white shadow-blue-500/40`}
           >
-            <MdOutlinePendingActions className='w-6 h-6 text-white' />
+            <MdOutlinePendingActions className="w-6 h-6 text-white" />
           </div>
-          <div className='p-4 text-right'>
-            <p className='block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600'>
-            Pending Task
+          <div className="p-4 text-right">
+            <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+              Pending Task
             </p>
-            <h4 className='block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900'>
+            <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
               {pendingTaskCount}
             </h4>
           </div>
         </div>
-       </div>
-        {/* Total Payment */}
-        <div className=" bg-bg-main">
-        <div className='relative flex flex-col bg-clip-border rounded-xl bg-bg-main text-white shadow-md '>
+      </div>
+      {/* Total Payment */}
+      <div className=" bg-bg-main">
+        <div className="relative flex flex-col bg-clip-border rounded-xl bg-bg-main text-white shadow-md ">
           <div
             className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute mt-2 grid h-16 w-16 place-items-center from-pink-600 to-pink-400 text-white shadow-pink-500/40`}
           >
-            <FaDollarSign className='w-6 h-6 text-white' />
+            <FaDollarSign className="w-6 h-6 text-white" />
           </div>
-          <div className='p-4 text-right'>
-            <p className='block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600'>
-            Total Payment
+          <div className="p-4 text-right">
+            <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600">
+              Total Payment
             </p>
-            <h4 className='block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900'>
+            <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
               ${totalPayments}
             </h4>
           </div>
         </div>
-        </div>
-        
       </div>
-  )
-}
+    </div>
+  );
+};
 BuyerStates.propTypes = {
   buyerStat: PropTypes.shape({
     pendingTaskCount: PropTypes.number,
@@ -77,4 +72,3 @@ BuyerStates.propTypes = {
 };
 
 export default BuyerStates;
-
--- src/pages/dashboard/worker/WorkerHome.jsx
+++ src/pages/dashboard/worker/WorkerHome.jsx
@@ -1,9 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../../../components/Loading";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import WorkerStates from "../../../components/worker/WorkerStates";
 
 
 const WorkerHome = () => {
+  const axiosSecure = useAxiosSecure()
+  const {user} = useAuth()
+  const {
+    data: workerStat = {},
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["worker-stat", user?.email],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/worker-stats/${user?.email}`);
+      return data;
+    },
+  });
+  if(isLoading) return <Loading/>
+  if(isError) console.log("Error in Worker Home", isError)
+    
   return (
     <div>
-      Worker home page
+     <WorkerStates workerStat={workerStat} />
     </div>
   )
 }
